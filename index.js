dcf13e0ce0fef510ddf8b40de10eada9a8cf8244
cindychen042 <gummybear15165@gmail.com>
2024-03-07 09:33:29 +1300

    Improve UI by adding styling and formatting

--- index.js
+++ index.js
@@ -7,71 +7,69 @@ const resetBtn = document.querySelector(".reset");
 const choiceBtns = document.querySelectorAll(".choices");
 const result = document.createElement("div");
 
+//function to random select and return computer's choice
 function getComputerChoice() {
   const choices = ["Rock", "Paper", "Scissors"];
 
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
+//function that return the result for each round base on the player's and computer's selection
 function playRound(playerSelection, computerSelection) {
   let roundOutcome = "";
-  playerSelection =
-    playerSelection.charAt(0).toUpperCase() +
-    playerSelection.slice(1).toLowerCase();
 
   if (playerSelection === computerSelection) {
-    roundOutcome = "It's a tie!";
+    roundOutcome = "<br>It's a tie!</br>";
   } else if (
     (playerSelection === "Rock" && computerSelection === "Scissors") ||
     (playerSelection === "Paper" && computerSelection === "Rock") ||
     (playerSelection === "Scissors" && computerSelection === "Paper")
   ) {
-    roundOutcome = `You win! ${playerSelection} beats ${computerSelection}`;
+    roundOutcome = `<br>You win! ${playerSelection} beats ${computerSelection}</br>`;
     playerScore += 1;
   } else {
-    roundOutcome = `You lose! ${computerSelection} beats ${playerSelection}`;
+    roundOutcome = `<br>You lose! ${computerSelection} beats ${playerSelection}</br>`;
     computerScore += 1;
   }
+  result.innerHTML = roundOutcome;
   return roundOutcome;
 }
 
 function updateGameDisplay() {
-
-  result.innerHTML += `Computer: ${computerScore}, Player: ${playerScore}`;
+  result.innerHTML += `<br>Computer: ${computerScore}, Player: ${playerScore}</br>`;
 
   if (playerScore >= WINNING_SCORE || computerScore >= WINNING_SCORE) {
     if (computerScore > playerScore) {
-      result.innerHTML +="Game Over, you lose!";
+      result.innerHTML += "<br>Game Over, you lose!</br>";
     } else if (computerScore < playerScore) {
-      result.innerHTML +="Congratulations, you win!";
+      result.innerHTML += "<br>Congratulations, you win!</br>";
     } else {
-      result.innerHTML +="Game Over! It's a tie!";
+      result.innerHTML += "<br>Game Over! It's a tie!</br>";
     }
-    choiceBtns.forEach((btn)=>{
+    choiceBtns.forEach((btn) => {
       btn.disabled = true;
-    })
+    });
   }
 }
 
-function resetGame(){
+function resetGame() {
   playerScore = 0;
   computerScore = 0;
-  result.innerHTML = '';
+  result.innerHTML = "";
 
-  choiceBtns.forEach((btn)=>{
+  choiceBtns.forEach((btn) => {
     btn.disabled = false;
-  })
+  });
 }
 
 choiceBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
     computerSelection = getComputerChoice();
-    playerSelection = btn.textContent;
+    playerSelection = btn.dataset.id;
     result.innerHTML = playRound(playerSelection, computerSelection);
     body.appendChild(result);
     updateGameDisplay();
   });
 });
 
-
-resetBtn.addEventListener("click",resetGame);
+resetBtn.addEventListener("click", resetGame);
